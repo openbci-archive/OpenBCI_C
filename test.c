fc37b236d905f2458e001aa37d478d75b8cc115b
Baffo32 <baffo32@gmail.com>
2016-08-25 08:06:19 +0000

    Build as a library; clean up makefile a little.

--- test.c
+++ test.c
@@ -1,32 +1,26 @@
 #include "openbci_c.h"
-#include "openbci_c.c"
 
 
-void main(){
+int main(){
   struct packet local_packet;
   int return_val = 0;
-  port = "/dev/ttyUSB0";
+  char * port = "/dev/ttyUSB0";
 
   find_port();
-  //set_port(port);
+  set_port(port);
 
 
   setup_port();
 
-  while(1){
-    switch(isStreaming){
-      case FALSE:
-          parse_strings();
-          sleep(5);
-          start_stream();
-          break;
+  parse_strings();
+  sleep(5);
+  start_stream();
 
-      case TRUE:
-          local_packet = streaming();
-          print_packet(local_packet);
-          break;
-    }
+  while(1){
+    local_packet = streaming();
+    print_packet(local_packet);
   }
 
+  return return_val;
 }
 
